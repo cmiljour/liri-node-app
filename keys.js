ec9a4d82fdd6bd4d91d6abbaf30b9324555f87ac
cmiljour <cormil@gmail.com>
2017-07-13 13:49:57 -0500

    framework

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'o99pcuWl0UaXpHLso0omaTJVB',
+  consumer_secret: 'TRVnmovHkQJzchOi9T4ZblJctShluvs7rx0Vcnn92fzC38EAPg',
+  access_token_key: '885530646755586049-xSk7SIa1LjRyLjNlPl5thYCeOG3E7k7',
+  access_token_secret: 'nvTBEjYG06v1cEQTtCh5WTXRPtzDU1oMNoJpaV5f9knZP',
+}
