9967449074b83b38efd1eb3e2c4bce39e4b4889f
cmiljour <cormil@gmail.com>
2017-07-14 10:02:21 -0500

    twitter done, spotify now

--- keys.js
+++ keys.js
@@ -1,8 +1,11 @@
-console.log('this is loaded');
-
 exports.twitterKeys = {
   consumer_key: 'o99pcuWl0UaXpHLso0omaTJVB',
   consumer_secret: 'TRVnmovHkQJzchOi9T4ZblJctShluvs7rx0Vcnn92fzC38EAPg',
   access_token_key: '885530646755586049-xSk7SIa1LjRyLjNlPl5thYCeOG3E7k7',
   access_token_secret: 'nvTBEjYG06v1cEQTtCh5WTXRPtzDU1oMNoJpaV5f9knZP',
+}
+
+exports.spotifyKeys = {
+  clientId_key: 'd41c0c8bc3cc4699b33b4233719247c4',
+  clientSecret_key: 'a33ec403501b4c9e99b660c32aee0131',
 }
--- liri.js
+++ liri.js
@@ -1,6 +1,41 @@
-var twitterSecret = require('./keys.js');
+var secretKeys = require('./keys.js');
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
 var userInput1 = process.argv[2];
 var userInput2 = process.argv[3];
 
-console.log(userInput1);
-console.log(userInput2);
+var client = new Twitter({
+  consumer_key: secretKeys.twitterKeys.consumer_key,
+  consumer_secret: secretKeys.twitterKeys.consumer_secret,
+  access_token_key: secretKeys.twitterKeys.access_token_key,
+  access_token_secret: secretKeys.twitterKeys.access_token_secret
+});
+
+var spotify = new Spotify({
+  id: secretKeys.spotifyKeys.clientId_key,
+  secret: secretKeys.spotifyKeys.clientSecret_key
+});
+
+if (!userInput1){
+    console.log("You didn't type anything after 'node liri.js' !")
+}
+
+else if (userInput1 === 'my-tweets') {
+    client.get('https://api.twitter.com/1.1/statuses/user_timeline.json', function(error, tweets, response) {
+        tweets.forEach(function(element) {
+            console.log(element.created_at + '     ' +  element.text);
+        });
+    });
+}
+
+else if (userInput1 === 'spotify-this-song') {
+    spotify.search({ type: 'track', query: userInput2}, function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        // data.tracks.items.forEach(function(element){
+        //     console.log(element.album.artists);
+        // });
+        console.log(data.tracks.items);
+    });
+}
